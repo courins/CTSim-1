540899243ccca3a4e9c17be456544d066a060c2c
Meng <febmeg@gmail.com>
2015-12-21 10:39:18 -0800

    beam harding temp

--- test/flame/temp_curve.m
+++ test/flame/temp_curve.m
@@ -13,7 +13,8 @@ clear data;
 
 %% Get image pixel that are not gas
 
-imgSub = imgKr - imgAir;
+kernelSize = 5;
+%imgSub = imgKr - imgAir;
 
 % segmentation
 solid = imgAir > 0.3;
@@ -21,30 +22,31 @@ solid = solid | imgKr > 0.3;
 
 % mophological blurring in all dimension
 final = solid;
+
 solid_blurred = solid; 
 
 % in x direction
 for i = 1 : size( imgAir, 1 )
-    solid_blurred( i, :, : ) = imdilate( solid(i,:,:) , ones(3) ); 
+    solid_blurred( i, :, : ) = imdilate( solid(i,:,:) , ones(kernelSize) ); 
 end
 final = final | solid_blurred;
 
 % in y direction
 for i = 1 : size( imgAir, 1 )
-    solid_blurred( :, i, : ) = imdilate( solid(:,i,:) , ones(3) ); 
+    solid_blurred( :, i, : ) = imdilate( solid(:,i,:) , ones(kernelSize) ); 
 end
 final = final | solid_blurred;
 
 % in z direction
 for i = 1 : size( imgAir, 3 )
-    solid_blurred( :, :, i ) = imdilate( solid(:,:,i) , ones(3) ); 
+    solid_blurred( :, :, i ) = imdilate( solid(:,:,i) , ones(kernelSize) ); 
 end
 final = final | solid_blurred;
 
 final = final | abs( imgSub ) > 0.02;
 
 % for slices that have porous media below system resolution
- final( :, :, 370 : end ) = false;
+ final( :, :, 380 : end ) = false;
 
 
 %% Now let's compute average density for each slice
@@ -57,22 +59,31 @@ y = [120 260];
 %final( 120:140,120:140,:) = true;
 
 att_curve = zeros( 1, size( imgAir, 3));
+att_curve_bhc = zeros( 1, size( imgAir, 3));
 
 for i = 1 : length( att_curve )
     
     slice = imgSub(x(1):x(2), y(1):y(2),i);
+    slice_bhc = imgSubBHC(x(1):x(2), y(1):y(2),i);
+    
     valid =  ~final( x(1):x(2), y(1):y(2),i);
     
     att_curve(i) = mean( slice( valid(:) ) );
-    
+    att_curve_bhc(i) = mean( slice_bhc( valid(:) ) );
 end
 
-figure; plot( att_curve ); 
+figure; plot( att_curve ); hold on;
+plot( att_curve_bhc, 'r' );
 xlabel 'slice #', ylabel 'attenuation';
 
 
 figure;
 slice = imgSub(:,end/2,:);
 slice( final(:,end/2,:) ) = 0;
-imagesc( squeeze( slice )' , [0 0.05] ); axis image, colorbar, colormap jet;
+imagesc( squeeze( slice )' , [0 0.02] ); axis image, colorbar, colormap jet;
 
+
+figure;
+slice = imgSubBHC(:,end/2,:);
+slice( final(:,end/2,:) ) = 0;
+imagesc( squeeze( slice )' , [0 0.02] ); axis image, colorbar, colormap jet;
--- test/flame/test_recon_bhc_meng.m
+++ test/flame/test_recon_bhc_meng.m
@@ -2,116 +2,46 @@
 load 'temp.mat';
 
 geom = loadProjectionGeometryCT( p );
+geom.detOffset(2) = -  geom.detOffset(2);
 
 spectrum = loadSpectraCT(p, geom, 2e6);
 
-Dir = 'D:\MATLAB\CTData\Dec_01_2015_Study\3ppi_sic_60KV_50mA_lowflow\';
+Dir = 'E:\Data\NasaFlame\Nov_10_2015_Study\3ppi_interface_60kV_50mA\';
 
 %% load air scan data
 
-dataPathAir = [Dir 'air_04' '\'];
-
-process_seq_file( Dir, 'air_04' );
-
+dataPathAir = [Dir 'air_cold_05' '\'];
+process_seq_file( Dir, 'air_cold_05' );
 sinoAttAir = loadTableTopData( dataPathAir, geom, 0, [701 800], [101 500] );
 
 % load burn scan data
 
-dataPath = [Dir 'burn_05' '\'];
-
-process_seq_file( Dir, 'burn_05' );
-
+dataPath = [Dir 'background_01' '\'];
+process_seq_file( Dir, 'background_01' );
 sinoAtt = loadTableTopData( dataPath, geom, 0, [701 800], [101 500] );
 
 
 %% first pass reconstruction
 
 sinoAttAirPoly = beamHardeningMaterialCorrection(sinoAttAir, spectrum, 'Quartz', 3 );
-
 imgAir = reconFBP( sinoAttAirPoly, geom, 'hamming' );
 
-%imgAir = reconFBP( sinoAttAir, geom, 'hamming' );
+sinoAttPoly = beamHardeningMaterialCorrection(sinoAtt, spectrum, 'Quartz', 3 );
+imgKr = reconFBP( sinoAttPoly, geom, 'hamming' );
 
-%clear sinoAttAirPoly;
+imgSub = imgKr - imgAir;
 
-%return;
-
+clear sinoAttAirPoly sinoAttPoly;
 
 %% second pass beam hardening correction
 
-mapTube = single( imgAir > 0.6 );
-
+mapTube = single( imgAir > 0.7 );
 sinoTube = forwardProjectMex( mapTube, geom ) ;
-
 sinoTube = imfilter3( sinoTube, fspecial('gaussian', [5 5], 1 ) );
 
 %%
 
-sinoDiff = sinoAtt - sinoAttAir;
+sinoSubBHC = beamHardeningMaterialCorrectionBurner(sinoAtt - sinoAttAir, (sinoAtt + sinoAttAir)/2, sinoTube, spectrum);
 
-sinoAttAirBHC = beamHardeningMaterialCorrectionBurner(sinoDiff, sinoAttAir, sinoTube, spectrum);
+imgSubBHC = reconFBP( sinoSubBHC, geom, 'hamming' );
 
-imgAir = reconFBP( sinoAttAirBHC, geom, 'hamming' );
-
-% 
-% % final reconstruction
-% 
-% imgAir = reconFBP( sinoAttAirBHC, geom, 'hamming' );
-% 
-% figure(21); 
-% if geom.reconSize(3) < 40
-%     imdisp( imgAir, [0 0.5]   );
-% else
-%     imdisp( squeeze(imgAir(end / 2, :, : ))', [0 0.8]   );
-% end
-
-
-%% first pass reconstruction
-
- %sinoAttPoly = beamHardeningMaterialCorrection(sinoAtt, spectrum, 'Quartz', 3 );
-
-imgKr = reconFBP( sinoAtt, geom, 'hamming' );
-
-%clear sinoAttPoly;
-
-%% second pass beam hardening correction
-
-% mapTube = single( imgKr > 0.6 );
-% 
-% sinoTube = forwardProjectMex( mapTube, geom ) ;
-% 
-% sinoTube = imfilter3( sinoTube, fspecial('gaussian', [5 5], 1 ) );
-% 
-% sinoAttBHC = beamHardeningMaterialCorrectionBurner(sinoAtt, sinoTube, spectrum);
-% 
-% % final reconstruction
-% 
-% imgKr = reconFBP( sinoAttBHC, geom, 'hamming' );
-% 
-% figure(22);
-% if geom.reconSize(3) < 40
-%     imdisp( imgKr, [0 0.5] );
-% else
-%      imdisp( squeeze(imgKr(end / 2, :, : ))', [0 0.8]   );
-% end
-
-%%
-figure(23)
-imagesc( squeeze(imgKr(end / 2, :, : ) - imgAir(end / 2, :, : ))', [0 0.1]   );
-
-
-return;
-
-%%
-imgKrReg = imgKr;
-
-[optimizer, metric] = imregconfig('monomodal');
-for i = 1 : size( imgAir, 3 )
-    if mod(i, 50) == 0
-        fprintf('(%i/%i)... ', i, size(imgAir, 3 ) );
-    end
-    slice = imregister(imgKr(:,:,i), imgAir(:, :, i), 'rigid', optimizer, metric);
-    imgKrReg( :,:,i) = slice;
-end
-
-figure(23); imdisp( imgKrReg(:,end/2,:) - imgAir(:,end/2,:)  , [-0.1 0.1] );
